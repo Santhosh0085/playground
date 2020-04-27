5e267aab301c067ba6348017e995c34c2dff3fea
Santhosh0085 <61708791+Santhosh0085@users.noreply.github.com>
2020-04-27 08:31:26 -0500

    Add files via upload

---
+++ String Randomizer/StrRandomizerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StrRandomizerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StrRandomizerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("973c1851-c994-4e29-bb76-3fbe84bf5acb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ String Randomizer/StrRandomizerTest/RandomAPITest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String_Randomizer;
+using System.Linq;
+
+namespace StrRandomizerTest
+{
+    /// <summary>
+    /// Test class containing test cases for string randomizers
+    /// </summary>
+    [TestClass]
+    public class RandomAPITest
+    {
+        /// <summary>
+        /// Test Random API randomizer
+        /// </summary>
+        [TestMethod]
+        public void Test_RandomAPIBasic()
+        {
+            StrRandomizer random = new StrRandomizer();
+            string input = "test";
+            string result = random.Shuffle(input, RandomizerType.Random);
+
+            // The shuffle works if
+            // 1) The input and result lengths are the same
+            // 2) The sorted string of input and and result are the same (same number of character counts)
+            Assert.IsTrue(result.Length == 4 && new string (input.OrderBy(c => c).ToArray()).Equals(new string(result.OrderBy(r => r).ToArray())));
+        }
+
+        /// <summary>
+        /// Test Crypto API randomizer
+        /// </summary>
+        [TestMethod]
+        public void Test_CryptoAPIBasic()
+        {
+            StrRandomizer random = new StrRandomizer();
+            string input = "test";
+            string result = random.Shuffle(input, RandomizerType.Crypto);
+
+            // The shuffle works if
+            // 1) The input and result lengths are the same
+            // 2) The sorted string of input and and result are the same (same number of character counts)
+            Assert.IsTrue(result.Length == 4 && new string(input.OrderBy(c => c).ToArray()).Equals(new string(result.OrderBy(r => r).ToArray())));
+        }
+
+    }
+}
